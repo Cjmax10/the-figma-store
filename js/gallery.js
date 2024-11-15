9b917cb58aabb6b6c946230f682727806cf336b6
cjmax <cjmaxdev@gmail.com>
2024-11-15 11:11:31 +0530

    URL CHANGES

--- js/gallery.js
+++ js/gallery.js
@@ -21,7 +21,7 @@ let cartCount;
 let cartValue;
 
 function fetchData() {
-    fetch('../data.json')
+    fetch('data.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
