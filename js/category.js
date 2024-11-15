460b64071cb33974b3d0419cd8eeac2fb0e75407
cjmax <cjmaxdev@gmail.com>
2024-11-15 11:06:40 +0530

    ULR CHANGES

--- js/category.js
+++ js/category.js
@@ -1,7 +1,7 @@
 const cardContainer = document.querySelector('.category-wrapper');
 
 function fetchData() {
-    fetch('../data.json')
+    fetch('data.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
